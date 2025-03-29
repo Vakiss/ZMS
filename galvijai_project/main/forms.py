7b7cded3de5667ba1a943bf335edc978cbe3391a
Vakaris Simanavičius <vakarissimanavi@gmail.com>
2025-03-29 13:51:24 +0200

    add labels to form

--- galvijai_project/main/forms.py
+++ galvijai_project/main/forms.py
@@ -5,5 +5,11 @@ class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
         fields = ['number', 'gender', 'color', 'name', 'weight']
-
+        labels = {
+            'numeris': 'Numeris',
+            'lytis': 'Lytis',
+            'spalva': 'Spalva',
+            'vadas': 'Vadas',
+            'svoris': 'Svoris',
+        }
 
