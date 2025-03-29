a516eca3981c83ef6a7954fe4feda0bc8a708d7c
Vakaris Simanavičius <vakarissimanavi@gmail.com>
2025-03-29 12:29:18 +0200

    updated urls and views

--- galvijai_project/main/views.py
+++ galvijai_project/main/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Animal
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return render(request, 'main/index.html')
@@ -11,3 +15,18 @@ def animal_list(request):
 def animal_detail(request, pk):
     animal = get_object_or_404(Animal, pk=pk)
     return render(request, 'main/animal_detail.html', {'animal': animal})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def some_protected_view(request):
+    return render(request, 'main/protected.html')
